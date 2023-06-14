abf587863c0906f504a2df282924baf405401c9d
Tomás Chaves <tomcchaves@gmail.com>
2023-06-14 15:00:43 -0300

    Lint.

--- app/backend/src/controllers/teams.controller.ts
+++ app/backend/src/controllers/teams.controller.ts
@@ -6,12 +6,12 @@ import teamService from '../services/Team.service';
 const getAllTeams = async (_req: Request, res: Response): Promise<Response> => {
   const { type, message } = await teamService.getAllTeams();
   return res.status(mapStatusHTTP(type)).json(message);
-}
+};
 
 const getTeamById = async (req: Request, res: Response): Promise<Response> => {
   const { id } = req.params;
   const { type, message } = await teamService.getTeamById(Number(id));
   return res.status(mapStatusHTTP(type)).json(message);
-}
+};
 
 export default { getAllTeams, getTeamById };
--- app/backend/src/services/Team.service.ts
+++ app/backend/src/services/Team.service.ts
@@ -1,19 +1,19 @@
-import TeamModel from "../database/models/SequelizeTeam";
+import TeamModel from '../database/models/SequelizeTeam';
 
 const getAllTeams = async () => {
   const teams = await TeamModel.findAll();
 
   if (teams.length === 0) return { type: 'NOT FOUND', message: 'Teams not found' };
-  
+
   return { type: 'SUCCESSFUL', message: teams };
 };
 
 const getTeamById = async (id: number) => {
   const team = await TeamModel.findByPk(id);
-  
+
   if (!team) return { type: 'NOT FOUND', message: 'Team not found' };
-  
+
   return { type: 'SUCCESSFUL', message: team };
-}
+};
 
 export default { getAllTeams, getTeamById };
