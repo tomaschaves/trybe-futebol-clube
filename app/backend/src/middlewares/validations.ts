f726867a1bc61247dae7f74b253e6c7e498e0be4
Tomás Chaves <tomcchaves@gmail.com>
2023-06-14 18:00:07 -0300

    Requisito 12.

--- app/backend/src/middlewares/validations.ts
+++ app/backend/src/middlewares/validations.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import { verifyToken } from '../auth/token';
 
 const loginValidation = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
@@ -19,4 +20,23 @@ const loginValidation = (req: Request, res: Response, next: NextFunction) => {
   return next();
 };
 
-export default loginValidation;
+const tokenValidation = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      return res.status(401).json({ message: 'Token not found' });
+    }
+
+    if (authorization !== undefined) {
+      const isValidToken = verifyToken(authorization);
+      res.locals.user = isValidToken;
+
+      return next();
+    }
+  } catch (error) {
+    res.status(401).json({ message: 'Token must be a valid token' });
+  }
+};
+
+export default { loginValidation, tokenValidation };
--- app/backend/src/routes/login.routes.ts
+++ app/backend/src/routes/login.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import loginController from '../controllers/login.controller';
-import loginValidation from '../middlewares/validations';
+import validations from '../middlewares/validations';
+import SequelizeUser from '../database/models/SequelizeUsers';
 
 const router = Router();
 
-router.post('/', loginValidation, loginController.loginUser);
+router.post('/', validations.loginValidation, loginController.loginUser);
+router.get('/role', validations.tokenValidation, async (_req, res) => {
+  const user = await SequelizeUser.findOne({ where: { email: res.locals.user.data } });
+  res.status(200).json({ role: user?.role });
+});
 
 export default router;
